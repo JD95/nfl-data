9e301cf0ffe79e0c988f787f175a8a7d982789d6
Jeff <jeffreydwyer95@hotmail.com>
2018-03-03 07:59:06 -0800

    Adds export_columns function
    Allows for the columns of the csv data file to be exported into
    another file.

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 #!~/usr/bin/env python
 
 import pandas as pd
-import numpy as np
+
+def export_columns(data_path, target_file):
+    """ Loads into pandas and all column names to file """
+    data = pd.read_csv(data_path)
+    columns = data.columns
+    contents = ''
+    for c in columns:
+        contents += str(c) + '\n'
+
+    with open(target_file, 'w') as f:
+        f.write(contents)
+
 
 def main():
-    data = pd.read_csv('data/nfl-play-by-play-2009-2016-v3.csv')
-    columns = data.columns
-    print(columns[0:5])
+    data_file = 'data/nfl-play-by-play-2009-2016-v3.csv'
+    target_file = 'data/columns.txt'
+    export_columns(data_file, target_file)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
